a422af362132d934c0c621416d8051d2060837e8
Ant0n Dmitrievich <stirin13@gmail.com>
2022-12-02 19:34:53 +0300

    task04

---
+++ task04/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите три целых числа");
+
+int a, b, c;
+
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToInt32(Console.ReadLine());
+
+if (a>b)
+{
+        if (a>c)
+        {
+    Console.WriteLine($"{a} самое большое число");
+    }
+    else Console.WriteLine($"{c} самое большое число");
+}
+else if (b>c)
+{
+    Console.WriteLine($"{b} самое большое число");
+}
+else Console.WriteLine($"{c} самое большое число");
