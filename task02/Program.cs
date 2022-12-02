fdf0cfd67592d7b57e18a9c71305ead6758dbd84
Ant0n Dmitrievich <stirin13@gmail.com>
2022-12-02 19:22:25 +0300

    task02

---
+++ task02/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите два целых числа");
+
+int a, b;
+
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+
+if (a>b)
+{
+    Console.WriteLine($"{a}, больше {b}");
+}
+else Console.WriteLine($"{b}, больше {a}");
