6af304925176394bcae97afd3aeae8db1bf5cde8
Ant0n Dmitrievich <stirin13@gmail.com>
2022-12-02 19:14:05 +0300

    task01

---
+++ task01/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите два целых числа");
+
+int a, b;
+
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b * b)
+{
+    Console.WriteLine("Да, первое число является квадратом второго");
+};
+else Console.WriteLine("Нет");
