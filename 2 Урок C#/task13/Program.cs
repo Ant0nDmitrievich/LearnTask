1f1008f975a967296fa3cdf1c4afc8d0fcf5c106
Ant0n Dmitrievich <stirin13@gmail.com>
2022-12-06 18:38:03 +0300

    task103

--- 2 Урок C#/task13/Program.cs
+++ 2 Урок C#/task13/Program.cs
@@ -1,14 +1,13 @@
-﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
-
-// 456 -> 5
-// 782 -> 8
-// 918 -> 1
-
-Console.WriteLine("Введите трехзначное число");
-
-int num = Convert.ToInt32(Console.ReadLine());
-int rem = num / 10;
-int res = rem % 10;
-
-Console.WriteLine($"Второе число равно {res}");
+﻿ int ThirdDigit(int number)
+        {
+            int result = -1;
+            if (number >= 100)
+            {
+                while (number > 999)
+                {
+                    number = number / 10;
+                }
+                result = number % 10;
+            }
+            return result; 
+        }
