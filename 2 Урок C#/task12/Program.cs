44ddf60d0a1bb28aa4ebe2a817bf97d550518203
Ant0n Dmitrievich <stirin13@gmail.com>
2022-12-06 18:06:11 +0300

    task12

---
+++ 2 Урок C#/task12/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите 2 числа:");
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int Multiple(int num1, int num2)
+    {
+        return num1 % num2;
+    }
+int res = Multiple(number1, number2);
+    if (res == 0)
+        {
+            Console.WriteLine("Кратно");
+        }
+else Console.WriteLine($"Не кратно, остаток {res}");
