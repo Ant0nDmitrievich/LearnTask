08bf20c5a45de4465b24d20ed268528447d44e5e
Ant0n Dmitrievich <stirin13@gmail.com>
2022-12-06 18:53:24 +0300

    task14

--- 2 Урок C#/task14/Program.cs
+++ 2 Урок C#/task14/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool MultiplicityTwoDigit(int num)
+    {
+        return num % 7 == 0 && num % 23 == 0;
+    }
+
+bool result = MultiplicityTwoDigit(number);
+string resultStr = result ? "Да" : "Нет";
+Console.WriteLine($"{number} -> {resultStr}");
