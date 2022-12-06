53cf9a6f2c1ea9dd158a261a50fc6ae210039237
Ant0n Dmitrievich <stirin13@gmail.com>
2022-12-06 17:43:39 +0300

    task11

---
+++ 2 Урок C#/task11/Program.cs
@@ -0,0 +1,17 @@
+﻿int number = new Random().Next(100, 1000);
+
+Console.WriteLine($"Случайное трехзначное число {number}");
+
+int MinusMiddle(int num)
+{
+    int firstDigit = num / 100;
+    int secondDigit = num % 10;
+    int result = firstDigit * 10 + secondDigit;
+    return result;
+}
+
+int number1 = MinusMiddle(number);
+
+Console.WriteLine($"Искомая цифра {number1}");
+
+
