b50702224ddc9608168f569b6c9a250bffff48bb
Ant0n Dmitrievich <stirin13@gmail.com>
2022-12-09 14:54:58 +0300

    task18 ready

--- 3 Урок C#/task18/Program.cs
+++ 3 Урок C#/task18/Program.cs
@@ -1,14 +1,13 @@
 ﻿Console.WriteLine("Введите номер четверти: ");
-int num = Convert.ToInt32(Console.ReadLine());
+string number = Console.ReadLine();
 
-string Range(int num)
+string Range(string num)
     {
-        if (num == 1) return "x > 0 и y > 0";
-        if (num == 2) return "x < 0 и y > 0";
-        if (num == 3) return "x < 0 и y < 0";
-        if (num == 4) return "x > 0 и y < 0";
-        return "Не принадлежит не одной из четвертей";
+        if (num == "1") return "x > 0 и y > 0";
+        if (num == "2") return "x < 0 и y > 0";
+        if (num == "3") return "x < 0 и y < 0";
+        if (num == "4") return "x > 0 и y < 0";
+        return "Введены некорректные данные";
     }
-string range = Range(num);
-//         Console.WriteLine(result);
+string range = Range(number);
 Console.WriteLine(range);
