120f82b8f6fae5031392f3d52df2f7f3496a5ed2
Ant0n Dmitrievich <stirin13@gmail.com>
2022-12-09 14:47:57 +0300

    task18

---
+++ 3 Урок C#/task18/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите номер четверти: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string Range(int num)
+    {
+        if (num == 1) return "x > 0 и y > 0";
+        if (num == 2) return "x < 0 и y > 0";
+        if (num == 3) return "x < 0 и y < 0";
+        if (num == 4) return "x > 0 и y < 0";
+        return "Не принадлежит не одной из четвертей";
+    }
+string range = Range(num);
+//         Console.WriteLine(result);
+Console.WriteLine(range);
