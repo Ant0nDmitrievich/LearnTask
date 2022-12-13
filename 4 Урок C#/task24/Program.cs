93143cdae916515ecd0d48608565aefb97925a69
Ant0n Dmitrievich <stirin13@gmail.com>
2022-12-13 12:24:19 +0300

    task24

---
+++ 4 Урок C#/task24/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите натуральное число");
+int a = Convert.ToInt32(Console.ReadLine());
+int Sum (int x)
+{
+    int y = 0;
+    for (int i = 1; i <= x; i++)
+    {
+        y += i;
+    }
+    return y;
+}
+if (a > 0)
+{
+    int sum = Sum(a);
+    Console.WriteLine($"Сумма чисел от 1 до {a} = {sum}");
+}
+else Console.WriteLine("Данные введены не корректно");
