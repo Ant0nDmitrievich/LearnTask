8b99114c85ee77ed5c58357d52fb63473bc49652
Ant0n Dmitrievich <stirin13@gmail.com>
2022-12-13 18:51:02 +0300

    task30 ready

--- 4 Урок C#/task30/Program.cs
+++ 4 Урок C#/task30/Program.cs
@@ -11,9 +11,10 @@
 int[] array = new int[8];
 void FillArray(int[] arr)
 {
+    Random rnd = new Random();
     for (int i = 0; i < arr.Length; i++)
     {
-        arr[i] = new Random().Next(0 , 2);
+        arr[i] = rnd.Next(0 , 2);
     }
 }
 Console.Write("[");
