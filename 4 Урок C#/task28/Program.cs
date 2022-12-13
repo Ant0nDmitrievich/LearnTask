7f29f71dffee8715ac667f20ea26436ed36f9900
Ant0n Dmitrievich <stirin13@gmail.com>
2022-12-13 17:26:14 +0300

    task28

---
+++ 4 Урок C#/task28/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите натуральное число");
+int a = Convert.ToInt32(Console.ReadLine());
+int Multicity (int x)
+{
+    int y = 1;
+    for (int i = 1; i <= x; i++)
+    {
+        y *= i;
+    }
+    return y;
+}
+if (a > 0)
+{
+    int multicity = Multicity(a);
+    Console.WriteLine($"Произведение чисел от 1 до {a} => {multicity}");
+}
+else Console.WriteLine("Данные введены не корректно");
