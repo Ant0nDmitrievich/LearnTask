2fad7319a76186006718551f16d04e4c09e0a2d6
Ant0n Dmitrievich <stirin13@gmail.com>
2022-12-13 12:58:51 +0300

    task26

--- 4 Урок C#/task26/Program.cs
+++ 4 Урок C#/task26/Program.cs
@@ -6,3 +6,15 @@
 
 Console.WriteLine("Введите целое число");
 int num = Convert.ToInt32(Console.ReadLine());
+int NumDigit(int n)
+{
+    int digit = 0;
+    while (n != 0)
+    {
+        n = n / 10;
+        digit++;
+    }
+    return digit;
+}
+int result = NumDigit(num);
+Console.WriteLine($"{num} -> {result}");
