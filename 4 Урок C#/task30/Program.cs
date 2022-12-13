c5ccba5cba2d2adafe011aa14ca496098a9da4ae
Ant0n Dmitrievich <stirin13@gmail.com>
2022-12-13 18:45:29 +0300

    task30

---
+++ 4 Урок C#/task30/Program.cs
@@ -0,0 +1,29 @@
+﻿// int[] array2 = { 3, 4, 6, 7};
+// int[] array1 = new int[5] { 5 , 3 , 3, 7 , 1};
+// int[] array = new int[8];
+
+
+// for (int i = 0; int < array.Length; i++)
+// {
+//     array[i] = i*i;
+// }
+
+int[] array = new int[8];
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0 , 2);
+    }
+}
+Console.Write("[");
+void PrintArray (int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]},");
+    }
+}
+FillArray(array);
+PrintArray(array);
+Console.Write("]");
