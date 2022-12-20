f90d503aa14d6daf07a641dd3e94271325b8e372
Ant0n Dmitrievich <stirin13@gmail.com>
2022-12-20 18:28:53 +0300

    task42

--- 6 Урок C#/task42/Program.cs
+++ 6 Урок C#/task42/Program.cs
@@ -4,3 +4,17 @@
 // 3 -> 11
 // 2 -> 10
 
+Console.WriteLine("Введите число, которое хотите преобразовать в двоичную систему исчисления.");
+int a = Convert.ToInt32(Console.ReadLine());
+
+string DecimalToBinary(int a)
+{
+    string result = string.Empty;
+    while (a > 0)
+    {
+        result = a % 2 + result;
+        a = a / 2;
+    }
+    return result;
+}
+Console.WriteLine(DecimalToBinary(a));
