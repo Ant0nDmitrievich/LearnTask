baf996602dfbb4c39778fc7a689a86322e7fd062
Ant0n Dmitrievich <stirin13@gmail.com>
2022-12-20 14:33:57 +0300

    task36

---
+++ 5 Урок C#/task36/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+int[] arr = new int[4];
+void FillArray(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(-100 , 100);
+    }
+}
+void PrintArray (int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("] ");
+}
+int GetSumNegativeElem(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+        if (arr[i] < 0) sum += arr[i];
+    return sum;
+}
+FillArray(arr);
+PrintArray(arr);
+int sumNegative = GetSumNegativeElem(arr);
+Console.WriteLine($"Сумма отрицательных чисел = {sumNegative}");
