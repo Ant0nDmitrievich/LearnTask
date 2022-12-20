1df0e3688efbd2e7283ee425777380047d418d85
Ant0n Dmitrievich <stirin13@gmail.com>
2022-12-20 19:23:46 +0300

    task45

--- 6 Урок C#/task45/Program.cs
+++ 6 Урок C#/task45/Program.cs
@@ -1,6 +1,14 @@
 ﻿// Напишите программу, которая будет создавать копию заданного массива
 // с помощью поэлементарного копирования.
 
+Console.WriteLine("Введите три числа: ");
+Console.Write("Размер массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Диапозон чисел от: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Диапаозон чисел до: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
 int[] CopyArray(int[] array)
 {
     int[] newArray = new int[array.Length];
@@ -10,13 +18,28 @@ int[] CopyArray(int[] array)
     }
     return newArray;
 }
-void PrintArray(int[] array)
+void PrintArray (int[] array)
 {
+    Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write(array[i]);
+        if(i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
     }
+    Console.Write("] ");
 }
-int[] arr = {5, 4, 6, 7};
-int[] newarr = CopyArray(arr);
-PrintArray(newarr);
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i]= rnd.Next(min, max + 1);
+    }
+return array;
+}
+int[] arr = CreateArrayRndInt(a, b, c);
+int[] newArr = CopyArray(arr);
+PrintArray(arr);
+Console.WriteLine();
+PrintArray(newArr);
