5279b4e9f5b2424ac093a80cdf61f1e6fc665cf1
Ant0n Dmitrievich <stirin13@gmail.com>
2022-12-20 19:57:26 +0300

    task41 no ready

--- 6 Урок C#/task41/Program.cs
+++ 6 Урок C#/task41/Program.cs
@@ -4,3 +4,26 @@
 // 0, 7, 8, -2, -2 -> 2
 
 // -1, -7, 567, 89, 223-> 3
+Console.WriteLine("Введите количество чисел: ");
+Console.Write("M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int EnterNum(int num)
+{
+    for (int i = 0; i < m; i++)
+    {
+        Console.WriteLine("Введите число: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+    }
+    return num;
+}
+int PositiveNum(int num)
+{
+    int sum = 0;
+    for (int i = 0; i < num; i++)
+        if (num > 0) sum++;
+    return sum;
+}
+int enternum = EnterNum(m);
+int positiveNum = PositiveNum(m);
+Console.WriteLine($"{positiveNum}");
--- 6 Урок C#/task43/Program.cs
+++ 6 Урок C#/task43/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения
+// b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите четыре числа: ");
+Console.Write("B1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("B2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("K1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("K2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+int y1 = k1 * x + b1;
+int y2 = k2 * x + b2;
+
