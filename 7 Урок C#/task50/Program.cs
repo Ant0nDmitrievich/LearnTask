d7843a9108bda0edb4d9aab4f871d8786676a4f8
Ant0n Dmitrievich <stirin13@gmail.com>
2022-12-23 19:45:25 +0300

    task50

--- 7 Урок C#/task50/Program.cs
+++ 7 Урок C#/task50/Program.cs
@@ -1,13 +1,41 @@
-﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+﻿//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
 // и возвращает значение этого элемента или же указание, что такого элемента нет.
-
 // Например, задан массив:
 
-// 1 4 7 2
+// 1  4  7  2
 
-// 5 9 2 3
+// 5  9  2  3
 
-// 8 4 2 4
+// 8  4  2  4
 
-// 1,7 -> такого элемента в массиве нет
+// 17 -> такого числа в массиве нет
 
+Console.Write("Введите строку: ");
+int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;
+Console.Write("Введите столбец: ");
+int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
+int n = 5;
+int m = 7;
+Random random = new Random();
+int[,] arr = new int[n, m];
+Console.WriteLine("Исходный массив: ");
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    Console.Write($"[");
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = random.Next(10, 99);
+        if (j < arr.GetLength(1) - 1) Console.Write($"{arr[i, j]}, ");
+        else Console.Write($"{arr[i, j]}");
+    }
+    Console.Write($"]");
+    Console.WriteLine();
+}
+if (pos1 < 0 | pos1 > arr.GetLength(0) - 1 | pos2 < 0 | pos2 > arr.GetLength(1) - 1)
+{
+    Console.WriteLine("Элемента не существует ");
+}
+else
+{
+    Console.WriteLine("Значение элемента массива = {0}", arr[pos1, pos2]);
+}
